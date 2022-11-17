9e776ad1af45c4c1bdd5c148f7250448b7641f61
Lukas Willems <lukas.willems@fom-net.de>
2022-11-17 20:45:43 +0100

    Add: Aufgabenstellung 6

---
+++ willems_Aufgabe6.py
@@ -0,0 +1,6 @@
+names = ["Luna","Simba","Nala","Leo","Liili","Charly","Lucy","Balu",
+"Mia","Findus","Mimi","Sammy","Maja","Felix","Bella","Loki",
+"Mila","Max","Frida","Tiger"]
+test = "0000t0t00t0t0t0t0t0t0t0tt0t0t00t0t0OttOt0t0ttt0t0t00t0t0t0t0t0t0t0"
+farben = ["Blau", "Rot", "Grün", "Gelb", "Orange",
+"Braun", "Weiß", "Lila", "Schwarz"]
