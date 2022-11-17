f3794a59bf13602c0452bb42226a672fa8b89d6b
Malik Achiri <malao.ach@gmx.de>
2022-11-17 20:45:44 +0100

    Add: Aufgabe 6e - Aufgabenstellung

---
+++ malik_Aufgabe6.py
@@ -0,0 +1,8 @@
+var = [1,3,5,7,9]
+varcopy = var[:]
+print("ID: ", id(var), # ID: 139919586427136
+"ID: ", id(varcopy)) # ID: 139919584695104
+var = "Klausur"
+varcopy = var[:]
+print("ID: ", id(var), # ID: 139919585342896
+"ID: ", id(varcopy)) # ID: 139919585342896
